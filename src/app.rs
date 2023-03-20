16cb5ea31bf1ca738d9767bfebb3ddbae7e3b8c9
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-03-20 22:38:58 +0100

    initialing main routine

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,50 @@
-use std::io;
+use std::time::{Duration, Instant};
 
 use anyhow::Result;
-use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use tui::{backend::CrosstermBackend, Terminal};
 
-pub fn run() -> Result<()> {
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+use crossterm::event::Event;
+use tui::{backend::Backend, Terminal};
 
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
+enum Control {
+    EntriesList,
+    TabsControl,
+    EntrNameTxt,
+    EntryContentTxt,
+}
 
+struct AppState {
+    active_control: Control,
+}
+
+impl AppState {
+    fn new(active_control: Control) -> Self {
+        Self { active_control }
+    }
+}
+
+pub fn run<B: Backend>(terminal: &mut Terminal<B>, tick_rate: Duration) -> Result<()> {
+    let mut last_tick = Instant::now();
+    let mut app = AppState::new(Control::TabsControl);
+    loop {
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = crossterm::event::read()? {
+                if !handle_input(key, &mut app)? {
+                    return Ok(());
+                }
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
+        }
+    }
     Ok(())
+}
+
+fn handle_input(key: crossterm::event::KeyEvent, app: &mut AppState) -> Result<bool> {
+    Ok(true)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use anyhow::{Ok, Result};
+use std::{io, time::Duration};
+
+use anyhow::Result;
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::{backend::CrosstermBackend, Terminal};
 
 mod app;
 
 fn main() -> Result<()> {
-    app::run()?;
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let tick_rate = Duration::from_millis(250);
+    app::run(&mut terminal, tick_rate)?;
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
     Ok(())
 }
