42642a8890b2cee54f5f866162ea71d3ba4fef2f
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-05-29 22:10:58 +0200

    Fix Export settings defaults
    Set true as default for confirmations when serialized

--- src/settings/export.rs
+++ src/settings/export.rs
@@ -6,8 +6,12 @@ use serde::{Deserialize, Serialize};
 pub struct ExportSettings {
     #[serde(default)]
     pub default_path: Option<PathBuf>,
-    #[serde(default)]
+    #[serde(default = "reutrn_true")]
     pub show_confirmation: bool,
+}
+
+fn reutrn_true() -> bool {
+    true
 }
 
 impl Default for ExportSettings {
